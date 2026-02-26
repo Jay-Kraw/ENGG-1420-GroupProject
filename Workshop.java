fb96c918bb14c5774d09f5190e27fa198188cbe5
Jay-Kraw <krawiecjacob@gmail.com>
2026-02-26 11:53:24 -0500

    Added Base Events Class and Exteded WorkShops Class

---
+++ Workshop.java
@@ -0,0 +1,21 @@
+
+public class Workshop extends Events {
+    String topic;
+
+
+
+    public String gettopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+   public Workshop(String title, String dateTime, String location, int capacity, boolean status,String topic)
+   {
+       super(title,dateTime,location,capacity,status);
+       this.topic=topic;
+
+   }
+}
